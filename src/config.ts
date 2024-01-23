86e68f87d83f2cdf8edb3185d2641bab7ed1e95e
reecen9696 <150649035+reecen9696@users.noreply.github.com>
2024-01-24 10:06:21 +1100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 export const MIST_CONVERSION = 1000000000;
 
+// List of Sui wallet addresses and how much Sui to send them.
+// 'to' is the wallet address to send Sui to.
+// 'amount' is how much Sui to send.
 export const airdrop = [
   {
     to: "0x3a845736adaecd77b46f733d5bab58df3a4301e01d4e2518c0efacd5b3c2269a",
